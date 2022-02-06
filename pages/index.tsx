d099dbf4d1b5ec3d85cd98e5d2ccff7d0bb96704
Otto Takkinen <otto.takkinen@gmail.com>
2022-02-06 20:53:52 +0200

    Add styled-components, theming and custom css reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,27 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import useTranslation from 'next-translate/useTranslation';
 import setLanguage from 'next-translate/setLanguage';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 3.5em;
+  color: ${({ theme }) => theme.colors.test};
+`;
 
 const Home: NextPage = () => {
   const { t } = useTranslation('common');
   return (
-    <div>
+    <>
       <Head>
         <title>{t('home:title')}</title>
         <meta name="description" content="Teekarius-sivu" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>{t('hello-world')}</h1>
+      <H1>{t('hello-world')}</H1>
       <button onClick={async () => await setLanguage('fi')}>FI</button>
       <button onClick={async () => await setLanguage('en')}>EN</button>
       <button onClick={async () => await setLanguage('sv')}>SV</button>
-    </div>
+    </>
   );
 };
 
