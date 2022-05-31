1ce214352e638c7a73d9e9d342b9c72b47a62be7
Otto Takkinen <otto.takkinen@gmail.com>
2022-05-31 23:32:20 +0300

    tpj kutsuinfo

---
+++ components/Kutsu.tsx
@@ -0,0 +1,279 @@
+import { useState } from 'react';
+import kutsu from '../public/images/kutsu.jpg';
+import sinetti from '../public/images/sinetti.png';
+import styled, { css, keyframes, useTheme } from 'styled-components';
+
+const Kutsu = () => {
+  const theme = useTheme();
+  const [trigger, setTrigger] = useState(false);
+  return (
+    <Container trigger={trigger}>
+      <CardContainer trigger={trigger}>
+        <CardTop trigger={trigger}>
+          <FrontTop>
+            <H2>Kutsu 150. Teekkariperinnejuhlaan</H2>
+            <p>
+              Teekkarius täyttää tänä vuonna huikeat 150 vuotta, jonka johdosta
+              haluaisimme kutsua Teidät juhlistamaan tätä ainutlaatuista
+              tapahtumaa kanssamme.
+              <br />
+              <br />
+              Teekkariperinnejuhlaa vietetään lauantaina 19.11.2022 Helsingin
+              Jäähallilla, osoitteessa Nordenskiöldinkatu 11-13.
+            </p>
+          </FrontTop>
+          <BackTop>
+            <StyledImg src={kutsu.src} alt="" />
+            <Button onClick={() => setTrigger(true)}>
+              <img
+                src={sinetti.src}
+                alt="Sinetti"
+                style={{ transform: 'scale(1.6) translate(3px, 2px)' }}
+              />
+            </Button>
+          </BackTop>
+        </CardTop>
+      </CardContainer>
+      <CardContainer trigger={trigger}>
+        <CardMiddle>
+          <p>
+            Juhlan hinta on AYY:n jäsenille ja opiskelijoille 100 € ja
+            valmistuneille 150 €. Silliaamiaisen hinta on jäsenille 25 € ja
+            valmistuneille 30 €.
+            <br />
+            <br />
+            Pukukoodina tässä juhlassa on juhlapuku akateemisin kunniamerkein.
+            Ilmoittautuminen aukeaa 22.6. klo 15.00 osoitteessa{' '}
+            <a
+              href="https://kide.app/TPJ150"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              kide.app/TPJ150
+            </a>
+          </p>
+        </CardMiddle>
+      </CardContainer>
+      <CardContainer trigger={trigger}>
+        <CardBottom trigger={trigger}>
+          <FrontBottom>
+            <p>
+              Vuosijuhlia innolla odottaen ja Teekkarius 150
+              vuosijuhlatoimikuntien puolesta,
+              <br />
+              Milja Koskela
+              <br />
+              Vuosijuhlatirehtööri
+            </p>
+          </FrontBottom>
+          <BackBottom>
+            <StyledImg src={kutsu.src} alt="" />
+          </BackBottom>
+        </CardBottom>
+      </CardContainer>
+    </Container>
+  );
+};
+
+const speed = 1;
+
+const upAnim = keyframes`
+  0% { transform: rotateX(0.1deg) }
+  100% {transform: rotateX(180deg)}
+`;
+
+const downAnim = keyframes`
+  0% { transform: rotateX(0deg) }
+  100% {transform: rotateX(-180deg)}
+`;
+
+const cardDown = keyframes`
+  0% { transform: translateY(0%) }
+  100% { transform: translateY(80%) }
+`;
+
+const hehku = keyframes`
+  0% { opacity: 0 }
+  100% { opacity: 1 }
+`;
+
+const Container = styled.div<{ trigger: boolean }>`
+  position: relative;
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 1rem;
+  transform-style: preserve-3d;
+  min-height: ${(p) => (p.trigger ? '54rem' : '20rem')};
+  transition: all 0.5s linear;
+  color: black;
+  text-align: center;
+
+  @media screen and (max-width: 800px) {
+    min-height: ${(p) => (p.trigger ? '44rem' : '20rem')};
+  }
+
+  & p {
+    font-size: clamp(1.3rem, 2vw, 1.5rem);
+
+    @media screen and (max-width: 800px) {
+      font-size: 0.8rem;
+    }
+  }
+`;
+
+const H2 = styled.h2`
+  margin-bottom: 1rem;
+  font-size: clamp(1.5rem, 3vw, 2rem);
+  text-align: center;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 100px;
+  background: #dba62a;
+  position: absolute;
+  border: none;
+  outline: none;
+  padding: 0;
+
+  border-radius: 50%;
+  cursor: pointer;
+  z-index: 800;
+  bottom: 0;
+  transform: translate(-50%, 50%);
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: -2px;
+    border-radius: 50%;
+    z-index: -1;
+    box-shadow: 0 0 25px 0px white;
+    animation: ${hehku} 0.7s infinite linear alternate;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 60px;
+    height: 60px;
+  }
+`;
+
+const CardContainer = styled.div<{ trigger: boolean }>`
+  position: absolute;
+  pointer-events: none;
+  width: 100%;
+  height: 20rem;
+  transform-style: preserve-3d;
+  transform: translateY(0%);
+  animation: ${(p) =>
+    p.trigger
+      ? css`
+          ${cardDown} ${speed * 0.5}s ease forwards
+        `
+      : 'none'};
+
+  @media screen and (max-width: 800px) {
+    height: 16rem;
+  }
+`;
+
+const Card = styled.div<{ trigger: boolean }>`
+  position: absolute;
+  pointer-events: initial;
+  z-index: 1;
+  width: 100%;
+  height: 80%;
+  transform-style: preserve-3d;
+  transform-origin: top;
+  transition: all ${speed}s ease;
+`;
+
+const CardMiddle = styled.div`
+  position: absolute;
+  pointer-events: initial;
+  padding: 0 1rem;
+  display: grid;
+  align-items: center;
+  z-index: -1;
+  background-color: ${(p) => p.theme.colors.polysteekki};
+  width: 100%;
+  height: 100%;
+`;
+
+const CardTop = styled(Card)`
+  z-index: 300;
+  transform: rotateX(0.1deg);
+  animation: ${(p) =>
+    p.trigger
+      ? css`
+          ${upAnim} ${speed}s ${speed * 0.2}s ease forwards
+        `
+      : 'none'};
+`;
+
+const FrontTop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  background: ${(p) => p.theme.colors.polysteekki};
+  transform-origin: bottom;
+  transform: rotateX(180deg) translateY(100%);
+  & h1 {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+`;
+
+const BackTop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform-origin: bottom;
+  backface-visibility: hidden;
+  text-align: center;
+  box-shadow: 0 8px 8px -8px rgba(255, 255, 255, 0.2);
+  transform: rotateX(0deg) translateY(0%);
+`;
+
+const CardBottom = styled(Card)`
+  transform-origin: bottom;
+  animation: ${(p) =>
+    p.trigger
+      ? css`
+          ${downAnim} ${speed}s ${0.6 * speed}s ease forwards
+        `
+      : 'none'};
+  bottom: 0;
+`;
+
+const FrontBottom = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  background: ${(p) => p.theme.colors.polysteekki};
+  transform-origin: bottom;
+  transform: rotateX(180deg) translateY(100%);
+`;
+
+const BackBottom = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform-origin: bottom;
+  backface-visibility: hidden;
+  overflow: hidden;
+  transform: rotateX(0deg) translateY(0%);
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export default Kutsu;
--- pages/tpj.tsx
+++ pages/tpj.tsx
@@ -1,172 +1,196 @@
 import React from 'react';
-import styled, { keyframes, useTheme } from 'styled-components';
+import styled from 'styled-components';
+import Layout from '../components/layout/Layout';
+import useTranslation from 'next-translate/useTranslation';
+import Head from 'next/head';
+import Kutsu from '../components/Kutsu';
 
-const TpjPage = () => {
-  const theme = useTheme();
+const MagicOfTechnologyPage = () => {
+  const { t } = useTranslation('events');
+
   return (
-    <Container>
-      <CardContainer>
-        <CardMiddle>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum,
-            aliquam! Unde nostrum tenetur perspiciatis distinctio veniam atque
-            laboriosam illo, maiores neque id sunt cumque alias harum. Error
-            architecto eius quod praesentium laborum nostrum delectus officia
-            quae totam. Expedita, delectus saepe.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum,
-            aliquam! Unde nostrum tenetur perspiciatis distinctio veniam atque
-            laboriosam illo, maiores neque id sunt cumque alias harum. Error
-            architecto eius quod praesentium laborum nostrum delectus officia
-            quae totam. Expedita, delectus saepe.
-          </p>
-        </CardMiddle>
-      </CardContainer>
-      <CardContainer style={{ transform: 'translateY(-50%)' }}>
-        <CardBottom>
-          <FrontBottom>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-              reprehenderit natus? Ipsa sed veritatis quisquam officia explicabo
-              fugiat delectus est molestias eveniet facilis! Id ullam dic nam
-              tempo est iolo cum ea, voluptatibus fuga quasi sint deserunt ipsam
-              alias quia soluta recusandae veniam magnam tenetur et nobis animi!
-              Repudiandae.
-            </p>
-            <br />
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Perspiciatis dolore sequi eveniet vel exercitationem maiores
-              nesciunt assumenda fugit nam hic!
-            </p>
-            <a href="#">Ilmoo ihmeessä</a>
-          </FrontBottom>
-          <BackBottom>
-            <h2>backside</h2>
-          </BackBottom>
-        </CardBottom>
-      </CardContainer>
-      <CardContainer style={{ transform: 'translateY(-50%)' }}>
-        <CardTop>
-          <FrontTop>
-            <h1>Kutsu nönnönnöö</h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-              reprehenderit natus? Ipsa sed veritatis quisquam officia explicabo
-              fugiat delectus est molestias eveniet facilis! Id ullam dicta nam
-              tempora est incidunt dolorem cumque ea, voluptatibus fuga quasi
-              sint deserunt ipsam alias quia soluta recusandae veniam magnam
-              tenetur et nobis animi! Repudiandae.
-            </p>
-          </FrontTop>
-          <BackTop>
-            <h2>backside</h2>
-          </BackTop>
-        </CardTop>
-      </CardContainer>
-    </Container>
+    <>
+      <Head>
+        <title>{t('Teekkariperinnejuhla')}</title>
+        <meta name="description" content="Teekkarius 150" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout navColor="portviini" imageUrl="" setImage={false}>
+        <EventsSection>
+          <Container>
+            <H1>Teekkariperinnejuhla</H1>
+            <KutsuContainer>
+              <Kutsu />
+            </KutsuContainer>
+            <InfoContainer>
+              <H2>Infoa</H2>
+              <P>
+                Ilmoittautuminen 150. Teekkariperinnejuhlaan on auki
+                kutsuvieraille 1.-15.6. ja jäsenille 22.6-31.7. Ilmoittautuminen
+                tapahtuu kide.appissa, jota varten tarvitset oman tilin, jotta
+                pystyt ostamaan sieltä tuotteita. Tili kannattaa luoda ennen
+                ilmoittautumisen aukeamista ja apua tähän saat seuraavasta
+                osoitteesta{' '}
+                <a href="https://kide.app/help/terms">
+                  https://kide.app/help/terms
+                </a>
+                . Kide.app toimii sekä verkkosivuilla sekä sovelluksena
+                kännykässä. Ilmoittautumislinkin löydät klikkaamalla yllä olevan
+                kutsun sinettiä!
+              </P>
+              <P>
+                Ilmoittautumisessa kysytään osallistujatiedot, sekä esimerkiksi
+                sähköpostiosoite, jonne laitetaan kaikki tuleva informaatio
+                juhliin liittyen. Jos sinulla on avec, niin hänen tulee
+                ilmoittautua myös erikseen, mutta saat ostettua maksimissaan
+                kaksi lippua, jolloin saat täytettyä tarvittavat tiedot samalla
+                ostokerralla myös avecillesi. Ilmotietoja saa muokattua
+                ilmoittautumisen jälkeen helposti kide.apissa 7.8. asti.
+                Huomaathan että ostotapahtuma on sitova.
+              </P>
+              <P>
+                Tapahtumassa on myös käytössä Aalto-yliopiston ylioppilaskunnan
+                Code of Conduct, joka jokaisen osallistujan tulee hyväksyä lipun
+                ostaakseen. Tämän avulla haluamme varmistaa että tapahtuma on
+                jokaiselle osallistujalle turvallinen ja mielekäs tapahtuma.
+                Emme hyväksy minkäänlaista kiusaamista tai syrjintää
+                tapahtumassa. Lisätietoa ylioppilaskunnan Code of Conductista
+                voit löytää{' '}
+                <a
+                  href="https://www.ayy.fi/fi/hallinnolliset-asiakirjat/turvallisemman-tilan-periaatteet"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  AYY:n nettisivuilta
+                </a>
+                .
+              </P>
+              <P>
+                Tervetuloa 150. Teekkariperinnejuhlaan! Lisää kysymyksiä voi
+                laittaa osoitteeseen <i>teekkarius150@ayy.fi</i>.
+              </P>
+            </InfoContainer>
+          </Container>
+        </EventsSection>
+      </Layout>
+    </>
   );
 };
 
-const upAnim = keyframes`
-  0% { transform: rotateX(0deg) }
-  100% {transform: rotateX(180deg)}
-`;
-
-const downAnim = keyframes`
-  0% { transform: rotateX(0deg) }
-  100% {transform: rotateX(-180deg)}
+const EventsSection = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  padding-top: 6rem;
+  background: rgb(61, 0, 36);
+  background: linear-gradient(
+    156deg,
+    rgba(61, 0, 36, 1) 0%,
+    rgba(145, 23, 31, 1) 100%
+  );
+  overflow: hidden;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.betoni};
+  position: relative;
 `;
 
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #000;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  max-width: 1024px;
+  width: 100%;
+  padding: 0 1.5em;
+  margin-bottom: 4rem;
+  min-height: 100vh;
+  z-index: 1;
+
+  @media screen and (max-width: 800px) {
+    padding: 0;
+  }
+`;
+
+const KutsuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform-style: preserve-3d;
   perspective: 1000px;
-`;
-
-const CardContainer = styled.div`
-  position: absolute;
-  width: 60vw;
-  height: 20vh;
-  transform-style: preserve-3d;
-  transform: translateY(-50%);
-`;
-
-const Card = styled.div`
-  position: absolute;
-  z-index: 1;
+  height: auto;
   width: 100%;
-  height: 80%;
-  transform-style: preserve-3d;
-  transform-origin: top;
-  transition: all 1s ease;
 `;
 
-const CardMiddle = styled.div`
-  position: absolute;
-  padding: 0 2rem;
-  z-index: 1;
-  background-color: teal;
+const InfoContainer = styled.section`
+  color: ${({ theme }) => theme.colors.betoni};
+  position: relative;
   width: 100%;
-  height: 100%;
+  padding: 2rem 8rem;
+  padding-bottom: 4rem;
+  gap: 2rem;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr;
+    padding: 0 1rem;
+  }
+  @media screen and (min-width: 800px) and (max-width: 1200px) {
+    padding: 2rem 4rem;
+  }
 `;
 
-const CardTop = styled(Card)`
-  z-index: 300;
-  animation: ${upAnim} 1s ease forwards;
+const H1 = styled.h1`
+  font-size: 3rem;
+  font-weight: bolder;
+  margin-bottom: 4rem;
+  font-family: 'KionaBold';
+  line-height: 1;
+  z-index: 2;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1.5rem;
+  }
 `;
 
-const FrontTop = styled.div`
-  position: absolute;
+const H2 = styled.h2`
+  margin-bottom: 2rem;
+  text-align: center;
+  line-height: 1;
+`;
+
+const P = styled.p`
+  margin-bottom: 1rem;
+  font-size: 1rem;
+
+  & > a {
+    color: inherit;
+  }
+`;
+
+const LargeText = styled.p`
+  margin-bottom: 1rem;
+  font-size: 1.5rem;
+  font-family: 'KionaBold';
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
   width: 100%;
-  height: 100%;
-  padding: 2rem;
-  background: teal;
-  transform-origin: bottom;
-  transform: rotateX(180deg) translateY(100%);
+  aspect-ratio: 16 / 9;
+  margin-bottom: -20rem;
+
+  @media screen and (max-width: 800px) {
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 800px) and (max-width: 1200px) {
+    margin-bottom: -10rem;
+  }
 `;
 
-const BackTop = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding: 2rem;
-  background: salmon;
-  transform-origin: bottom;
-  backface-visibility: hidden;
-  transform: rotateX(0deg) translateY(0%);
+const InfoTextContainer = styled.div`
+  margin: 1rem;
+  color: ${({ theme }) => theme.colors.betoni};
 `;
 
-const CardBottom = styled(Card)`
-  transform-origin: bottom;
-  animation: ${downAnim} 1s 1s ease forwards;
-  bottom: 0;
-`;
-
-const FrontBottom = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding: 2rem;
-  background: teal;
-  transform-origin: bottom;
-  transform: rotateX(180deg) translateY(100%);
-`;
-
-const BackBottom = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding: 2rem;
-  background: salmon;
-  transform-origin: bottom;
-  backface-visibility: hidden;
-  transform: rotateX(0deg) translateY(0%);
-`;
-
-export default TpjPage;
+export default MagicOfTechnologyPage;
